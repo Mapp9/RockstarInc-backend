5c228b60c5a681d3bb0e7c1666bb4a05dfae5048
Mapp9 <mpp.adso@gmail.com>
2024-07-30 11:05:09 -0500

    Primer commit con el backend

---
+++ src/main/java/com/rockstarinc/RIecom/RIecomApplication.java
@@ -0,0 +1,13 @@
+package com.rockstarinc.RIecom;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RIecomApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RIecomApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/rockstarinc/RIecom/RIecomApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rockstarinc.RIecom;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RIecomApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
