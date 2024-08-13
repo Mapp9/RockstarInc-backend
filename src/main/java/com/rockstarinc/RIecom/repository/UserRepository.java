1efbc5e27bb4da46965aeecff15cf2694fba0510
Mapp9 <mpp.adso@gmail.com>
2024-08-13 00:19:32 -0500

    add cart order and cart items entity

--- src/main/java/com/rockstarinc/RIecom/repository/UserRepository.java
+++ src/main/java/com/rockstarinc/RIecom/repository/UserRepository.java
@@ -5,14 +5,14 @@ import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-import com.rockstarinc.RIecom.entity.user;
+import com.rockstarinc.RIecom.entity.User;
 import com.rockstarinc.RIecom.enums.UserRole;
 
 @Repository
-public interface UserRepository extends JpaRepository<user, Long>{
+public interface UserRepository extends JpaRepository<User, Long>{
 
     // Método personalizado para buscar un usuario por su email
-    Optional<user> findFirstByEmail(String email);
+    Optional<User> findFirstByEmail(String email);
     // Método personalizado para buscar un usuario por su rol.
-    user findByRole(UserRole userRole);
+    User findByRole(UserRole userRole);
 }
